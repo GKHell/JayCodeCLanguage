9a5c8f3ec10f35daf6fea49ddab89afe5467006c
GK Sir <hellofcode@hell.you>
2021-06-29 18:25:51 +0530

    Tue29Jun202106:25:51PM+0530

--- Constant.c
+++ Constant.c
@@ -6,13 +6,23 @@
 */
 #include<stdio.h>
 // #include<conio.h>
+
+#define PI 3.14  // way 2
 int main()
 {
 
-	const int a=10; // constant
+	const int A=10; // constant // way1
 	// clrscr();
 		
-		a = 100;
-		printf("value of a : %d\n", a);
+		// A = 100; //gives error
+		// PI = 34; //gives error
+
+		printf("value of A : %d\n", A);
+		printf("value of PI : %.2f\n",PI);
+		//----------------try to break the weak constant
+		#undef PI
+				#define PI 500
+		printf("value of PI : %d\n",PI);
+
 	// getch();
 }
