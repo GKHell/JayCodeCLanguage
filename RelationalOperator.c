794f787e2f7f586783242f4fd95ac3ef038381b3
GK Sir <hellofcode@hell.you>
2021-07-02 18:05:42 +0530

    Fri02Jul202106:05:42PM+0530

---
+++ RelationalOperator.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+// #include<conio.h>
+void main()
+{
+	int a,b;
+	// clrscr();
+	
+
+	printf("Enter 2 numbers : ");
+	scanf("%d,%d",&a,&b);
+
+	if(1)
+	{
+		printf("%d is greater than %d\n",a,b);
+	}
+	// getch();
+
+
+}
