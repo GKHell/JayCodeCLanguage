cfd75d8ac84ce95627cbd7d5df467053e078b7b8
GK Sir <hellofcode@hell.you>
2021-07-02 18:59:28 +0530

    Fri02Jul202106:59:27PM+0530

--- RelationalOperator.c
+++ RelationalOperator.c
@@ -6,14 +6,53 @@ void main()
 	int a,b;
 	// clrscr();
 	
-
 	printf("Enter 2 numbers : ");
 	scanf("%d,%d",&a,&b);
 
-	if(1)
+	// printf("value : %d\n",a>b); //0 means false , non-zero true.
+	if(a>b)
 	{
-		printf("%d is greater than %d\n",a,b);
+		printf("true : %d is greater than %d\n",a,b);
 	}
+	else
+	{
+		printf("false : %d is not greater than %d\n",a,b);
+	}
+
+	printf("-------------------------\n");
+	if(a<b)
+		printf(" true : %d is less than %d\n",a,b);
+	else
+		printf("false : %d is not less than %d\n",a,b);
+
+	printf("-------------------------\n");
+
+	if(a>=b)
+		printf("true : %d is greater than or equals to %d\n",a,b);
+	else
+		printf("false : %d is not greater than or equals to %d\n",a,b);
+
+	printf("-------------------------\n");
+
+	if(a<=b)
+		printf("true : %d is less than equals to %d\n",a,b);
+	else
+		printf("false : %d is not less than equals to %d\n",a,b);
+	printf("-------------------------\n");
+
+
+	if(a==b)
+		printf("true : %d is equals to %d\n",a,b);
+	else
+		printf("false : %d is not equals to %d\n",a,b);
+	printf("-------------------------\n");
+
+	if(a!=b)
+		printf("true : %d is not equals to %d\n",a,b);
+	else
+		printf("false : %d is equals to %d\n",a,b);
+
+
 	// getch();
 
 
