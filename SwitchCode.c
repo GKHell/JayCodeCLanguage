40d340d4725a7588f206d3b71e56e87c9fb53526
GK Sir <hellofcode@hell.you>
2021-07-06 18:28:49 +0530

    Tue06Jul202106:28:48PM+0530

--- SwitchCode.c
+++ SwitchCode.c
@@ -9,12 +9,13 @@ int main()
 
 	switch(choice)
 	{
-		case 1:printf("Monday\n");
-		case 2:printf("Tuesday\n");
-		case 3:printf("Wednesday\n");
-		case 4:printf("Thursday\n");
-		case 5:printf("Friday\n");
-		case 6:printf("saturday\n");
-		case 7:printf("Sunday\n");
+		case 1:printf("Monday\n");break;
+		case 2:printf("Tuesday\n");break;
+		case 3:printf("Wednesday\n");break;
+		case 4:printf("Thursday\n");break;
+		case 5:printf("Friday\n");break;
+		case 6:printf("saturday\n");break;
+		case 7:printf("Sunday\n");break;
+		default:printf("invalid input..");
 	}
 }
