344da3fa13eff54330c1c0664d03cbdcc30dcce2
GK Sir <hellofcode@hell.you>
2021-07-08 18:23:43 +0530

    Thu08Jul202106:23:43PM+0530

---
+++ BackwardGOTO.c
@@ -0,0 +1,16 @@
+
+
+
+
+								Line 1;
+								Line 2;
+						goto me
+								Line 3;
+								Line 4;
+								Line 5;
+								Line 6;
+								Line 7;
+							me : 
+								Line 8;
+								Line 9;
+								Line 10;
