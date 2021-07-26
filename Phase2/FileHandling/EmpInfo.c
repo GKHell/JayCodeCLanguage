b4bfb88408d485f77c96c1b11dae8822b5616945
GK Sir <hellofcode@hell.you>
2021-07-26 18:59:32 +0530

    Mon26Jul202106:59:32PM+0530

---
+++ Phase2/FileHandling/EmpInfo.c
@@ -0,0 +1,43 @@
+
+#include<stdlib.h>
+#include<stdio.h>
+FILE *fp;
+char fileName[100];
+struct Employee{
+
+	int emp_id;
+	char name[100];
+	int salary;
+}emp;
+
+void getData()
+{
+	printf("Enter emp_id : ");
+	scanf("%d",&emp.emp_id);
+
+	printf("Enter name : ");
+	scanf("%s",emp.name);
+	 
+
+	printf("Enter salary : ");
+	scanf("%d",&emp.salary);
+
+	 
+	printf("Enter fileName : ");
+	scanf("%s",fileName);
+	printf("0----------");
+}
+
+void storeInFile()
+{
+	fp = fopen(fileName,"w");
+
+	fprintf(fp, "%4d | %10s | %d\n",emp.emp_id,emp.name,emp.salary);
+}
+
+void main()
+{
+
+	getData();
+	storeInFile();
+}
