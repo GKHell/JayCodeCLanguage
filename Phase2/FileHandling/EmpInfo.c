acae33401eaae3409b2de185ae80f78ec831bb10
GK Sir <hellofcode@hell.you>
2021-07-27 18:20:16 +0530

    Tue27Jul202106:20:16PM+0530

--- Phase2/FileHandling/EmpInfo.c
+++ Phase2/FileHandling/EmpInfo.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 FILE *fp;
-char fileName[100];
+char fileName[100] = "EmployeeCode.txt";
 struct Employee{
 
 	int emp_id;
@@ -23,21 +23,54 @@ void getData()
 	scanf("%d",&emp.salary);
 
 	 
-	printf("Enter fileName : ");
-	scanf("%s",fileName);
-	printf("0----------");
+	// printf("Enter fileName : ");
+	// scanf("%s",fileName);
+	// printf("0----------");
 }
 
 void storeInFile()
 {
-	fp = fopen(fileName,"w");
+	getData();
+	fp = fopen(fileName,"a");
 
 	fprintf(fp, "%4d | %10s | %d\n",emp.emp_id,emp.name,emp.salary);
+
+	fclose(fp);
+}
+
+void displayFileContent()
+{
+	fp = fopen(fileName,"r");
+	char ch;
+
+	while((ch=getc(fp))!=EOF)
+	{
+		printf("%c",ch);
+	}
+	fclose(fp);
+
 }
 
 void main()
 {
 
-	getData();
-	storeInFile();
+	int choice;
+ 
+ 	printf("  Enter 1 for store data : \n");
+ 	printf("Enter 2 for display data : \n");
+ 	printf("        Enter 3 for exit : \n");
+
+ 	do
+ 	{
+ 		printf("\nEnter choice : ");
+ 		scanf("%d",&choice);
+
+ 		switch(choice)
+ 		{
+ 			case 1: storeInFile();break;
+ 			case 2: displayFileContent();break;
+ 			case 3: exit(0);break;
+ 		   default: printf("Invalid choice..");
+ 		}
+ 	}while(choice!=3);
 }
